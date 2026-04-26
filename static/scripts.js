9bd5bf226c30d355121a38f80a9361507b6d3be2
vpak-mellon <vpak@andrew.cmu.edu>
2026-04-25 22:02:05 -0400

    Add styles and confirmation page for form

--- static/scripts.js
+++ static/scripts.js
@@ -5,3 +5,18 @@ menuIcon.addEventListener("click", () => {
     console.log(navbar.classList);
     navbar.classList.toggle("active");
 })
+
+// GET EARLY ACCESS PAGE:
+// Check if form is filled out and submit is hit, redirect to confirmation page
+if (window.location.pathname.includes("access.html")) {
+    document.querySelector(".form-container").addEventListener("submit", function(e) {
+        e.preventDefault(); // stop default form submission
+        const firstNameField = document.getElementById("first-name").value.trim();
+        const lastNameField = document.getElementById("last-name").value.trim();
+        const emailField = document.getElementById("email-address").value.trim();
+        if (firstNameField !== "" && lastNameField !== "" && emailField !== "") {
+            window.location.href = "success.html";
+            console.log("REDIRECT");
+        }
+    });
+}
